ceed2a38aac454e12ccba9f93f203ec46f1e0a92
Nicklas Ridewing <nicklas@ridewing.se>
2015-02-10 17:32:04 +0100

    Removed old ugly temp 'routing'

--- init.php
+++ init.php
@@ -33,17 +33,6 @@ $superPowers = new \SuperPowers\SuperPowers();
 if(!SUPERPOWERS_AJAX){
   add_action( 'init', function() use($superPowers)
   {
-    add_rewrite_rule('game/([^/]+)(/news)/?$', 'index.php?game=$matches[1]&subview=news', 'top');
-    add_rewrite_rule('game/([^/]+)(/review)/?$', 'index.php?game=$matches[1]&subview=review', 'top');
-    add_rewrite_rule('game/([^/]+)(/videos)/?$', 'index.php?game=$matches[1]&subview=videos', 'top');
-
-    add_rewrite_rule('user/([^/]+)/?$', 'index.php?type=user&user=$matches[1]', 'top');
-
-    add_rewrite_tag('%type%', '([^&]+)');
-    add_rewrite_tag('%user%', '([^&]+)');
-    add_rewrite_tag('%subview%', '([^&]+)');
-
-
     $superPowers->boot();
   });
 }
